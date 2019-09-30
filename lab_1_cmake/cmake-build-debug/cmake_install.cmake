a9d380bce1707a4b57be98cd4ba5b2f845276b03
fenix-125 <yurijpasichnyk11@gmail.com>
2019-09-30 14:02:40 +0300

    lab_1: fixed bug in lib compilation Makefile

--- lab_1_cmake/cmake-build-debug/cmake_install.cmake
+++ lab_1_cmake/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/fenix/pr/POK/lab_1_cmake
+# Install script for directory: /home/fenix/pr/subj/POK/lab_1_cmake
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/fenix/pr/POK/lab_1_cmake/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/fenix/pr/subj/POK/lab_1_cmake/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
