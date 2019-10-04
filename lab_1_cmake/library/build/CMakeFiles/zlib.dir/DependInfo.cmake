96a60110282520625439af750f0f6ab5cd9a910e
fenix-125 <yurijpasichnyk11@gmail.com>
2019-10-04 17:32:34 +0300

    lab_1: fexed Cmake file for examples

---
+++ lab_1_cmake/library/build/CMakeFiles/zlib.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/adler32.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/adler32.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/compress.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/compress.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/crc32.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/crc32.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/deflate.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/deflate.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/gzclose.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/gzclose.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/gzlib.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/gzlib.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/gzread.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/gzread.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/gzwrite.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/gzwrite.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/infback.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/infback.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/inffast.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/inffast.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/inflate.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/inflate.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/inftrees.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/inftrees.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/trees.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/trees.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/uncompr.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/uncompr.c.o"
+  "/home/fenix/pr/subj/POK/lab_1_cmake/library/zutil.c" "/home/fenix/pr/subj/POK/lab_1_cmake/library/build/CMakeFiles/zlib.dir/zutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
