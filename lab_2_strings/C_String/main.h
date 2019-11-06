7c2af33d8988f98a7556126ee20e96d45776a99c
fenix-125 <yurijpasichnyk11@gmail.com>
2019-11-06 12:47:14 +0200

    updated the resul of lab 2

---
+++ lab_2_strings/C_String/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char to_lower(char c);
+int is_alpha(char c);
+void insertionSort(char *elements, size_t a, size_t b);
+void my_str_sort_words(my_str_t *str);
+#endif // MAIN_H
