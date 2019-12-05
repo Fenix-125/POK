a1f0b7e0fff2bdff05e7514a77bf4255e5b42d03
fenix-125 <yurijpasichnyk11@gmail.com>
2019-12-05 03:23:33 +0200

    add servo, small 4 phase stem motor, counter, blinker projects

---
+++ servo/cmake-build-debug/CMakeFiles/servo.elf.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj"
+  "CMakeFiles/servo.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj"
+  "CMakeFiles/servo.elf.dir/Src/gpio.c.obj"
+  "CMakeFiles/servo.elf.dir/Src/main.c.obj"
+  "CMakeFiles/servo.elf.dir/Src/stm32f3xx_hal_msp.c.obj"
+  "CMakeFiles/servo.elf.dir/Src/stm32f3xx_it.c.obj"
+  "CMakeFiles/servo.elf.dir/Src/sys.c.obj"
+  "CMakeFiles/servo.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/servo.elf.dir/Src/system_stm32f3xx.c.obj"
+  "CMakeFiles/servo.elf.dir/Src/tim.c.obj"
+  "CMakeFiles/servo.elf.dir/startup/startup_stm32f303xc.s.obj"
+  "servo.elf"
+  "servo.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/servo.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
