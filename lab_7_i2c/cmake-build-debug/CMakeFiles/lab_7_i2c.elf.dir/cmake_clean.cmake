abca30da0b7810743b985ab4459837fc7e336071
Fenix-125 <yurijpasichnyk11@gmail.com>
2019-12-29 16:18:43 +0200

    lab7 init

---
+++ lab_7_i2c/cmake-build-debug/CMakeFiles/lab_7_i2c.elf.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/gpio.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/i2c.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/l3gd20.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/lsm303dlhc.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/main.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/stm32f3_discovery.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/stm32f3_discovery_accelerometer.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/stm32f3_discovery_gyroscope.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/stm32f3xx_hal_msp.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/stm32f3xx_it.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/sys.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/system_stm32f3xx.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/Src/usart.c.obj"
+  "CMakeFiles/lab_7_i2c.elf.dir/startup/startup_stm32f303xc.s.obj"
+  "lab_7_i2c.elf"
+  "lab_7_i2c.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/lab_7_i2c.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
